a0cc50a2da4f1550598a57b0765fc35f4a8facba
amarteyfio <amarteyfio>
2022-11-05 23:22:56 +0000

    init_commit

---
+++ Classes/room_class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Amarteyfio
+ * @version 1.0
+ * This class contains general functions for database operations
+ */
+
+//require db_class
+require("../Settings/db_class.php");
+
+class room_class extends db_connection{
+    
+    //--ADD ROOM--//
+
+
+
+
+    //--SELECT ONE ROOM--//
+
+
+
+    //--DELETE ROOM--//
+}
+?>
